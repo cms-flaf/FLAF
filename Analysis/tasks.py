554e0d8fbbc3745dc9f4ae84bd5352eb0615cd38
Valeria D'Amante <valeriadamante@gmail.com>
2025-10-27 22:45:12 +0100

    fixed bugs in hist naming

--- Analysis/tasks.py
+++ Analysis/tasks.py
@@ -515,7 +515,7 @@ class HistFromNtupleProducerTask(Task, HTCondorWorkflow, law.LocalWorkflow):
                 "--channels",
                 channels,
                 "--var",
-                var_name,
+                var,
                 "--sample_name",
                 sample_name,
                 sample_name,

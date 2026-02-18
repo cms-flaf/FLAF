aed5e740018ec4b566a2d06f6e51daa98160143f
Konstantin <konstantin.androsov@gmail.com>
2026-02-18 11:17:55 +0100

    more formatting

--- AnaProd/MergeAnaTuples.py
+++ AnaProd/MergeAnaTuples.py
@@ -17,8 +17,7 @@ from Corrections.CorrectionsCore import central, getSystName, getScales
 from FLAF.AnaProd.anaTupleProducer import DefaultAnaCacheProcessor
 from FLAF.Common.Utilities import DeserializeObjectFromString
 
-ROOT.gInterpreter.Declare(
-    """
+ROOT.gInterpreter.Declare("""
     struct EventDuplicateFilter {
         using LumiEventMapType = std::map<unsigned int, std::set<unsigned long long>>;
         using EventMapType = std::map<unsigned int, LumiEventMapType>;
@@ -53,8 +52,7 @@ ROOT.gInterpreter.Declare(
         std::shared_ptr<EventMapType> eventMap;
         std::shared_ptr<std::mutex> eventMap_mutex;
     };
-    """
-)
+    """)
 
 
 def combineAnaCaches(anaCaches, processors):

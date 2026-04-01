4f775a774e475b46e65f317e2dbd0261e54fbbf8
Valeria D'Amante <vdamante@cern.ch>
2026-04-01 21:32:09 +0200

    restored 2024 and added 2025 for HHBtagScores

--- include/HHbTagScores.h
+++ include/HHbTagScores.h
@@ -21,7 +21,9 @@ inline int PeriodToHHbTagInput(int version, Period period) {
         {{1, Period::Run3_2023}, 2018},
         {{1, Period::Run3_2023BPix}, 2018},
         {{1, Period::Run3_2024}, 2018},
-        {{1, Period::Run3_2024},52018},
+        {{1, Period::Run3_2024},2018},
+        {{1, Period::Run3_2025},2018},
+
         // v2
         {{2, Period::Run2_2016_HIPM}, 2016},
         {{2, Period::Run2_2016}, 2016},
@@ -32,7 +34,9 @@ inline int PeriodToHHbTagInput(int version, Period period) {
         {{2, Period::Run3_2023}, 2018},
         {{2, Period::Run3_2023BPix}, 2018},
         {{2, Period::Run3_2024}, 2018},
-        {{2, Period::Run3_2024},52018},
+        {{2, Period::Run3_2024},2018},
+        {{2, Period::Run3_2025},2018},
+
         // v3
         {{3, Period::Run2_2016_HIPM}, 0},
         {{3, Period::Run2_2016}, 0},

c279584e1b85fa9f1049f47980cb8ef1d6ad2d9b
Devin Aebi <45836403+aebid@users.noreply.github.com>
2026-07-14 12:35:50 -0500

    Refactor histTuple variable assignments for clarity

--- Common/Setup.py
+++ Common/Setup.py
@@ -360,13 +360,13 @@ class Setup:
 
         self.histTuple_flavor = self.global_params["histTuple_flavor"]
         print(f"Using histTuple flavor {self.histTuple_flavor}")
+        histTuple_plot_vars = self.global_params["histTuple_flavors"][self.histTuple_flavor]["variables"]
         self.histTuple_plot_vars = set(
-            self.global_params["histTuple_flavors"][self.histTuple_flavor]["variables"]
+            histTuple_plot_vars if isinstance(histTuple_plot_vars, list) else histTuple_plot_vars.keys()
         )
+        histTuple_fullres_vars = self.global_params["histTuple_flavors"][self.histTuple_flavor]["fullResolution_variables"]
         self.histTuple_fullres_vars = set(
-            self.global_params["histTuple_flavors"][self.histTuple_flavor][
-                "fullResolution_variables"
-            ]
+            histTuple_plot_vars if isinstance(histTuple_fullres_vars, list) else histTuple_fullres_vars.keys()
         )
         self.histTuple_vars = (
             self.histTuple_plot_vars | self.histTuple_fullres_vars
